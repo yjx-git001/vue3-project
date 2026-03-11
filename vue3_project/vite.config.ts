7c4fe857a2aeb9296062a62cba0ecfac3aa073f9
yinjuxin <3307852286@qq.com>
2026-03-11 15:03:54 +0800

    修复问题

--- vue3_project/vite.config.ts
+++ vue3_project/vite.config.ts
@@ -6,7 +6,6 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/vue3-project/',
   plugins: [
     vue(),
     vueDevTools(),
