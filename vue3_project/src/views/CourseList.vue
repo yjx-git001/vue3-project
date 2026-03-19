dfaba0b9e34be491beeb42a7ce5df6dcf80561b2
yinjuxin <3307852286@qq.com>
2026-03-19 18:06:35 +0800

    fix

--- vue3_project/src/views/CourseList.vue
+++ vue3_project/src/views/CourseList.vue
@@ -359,6 +359,7 @@ onMounted(() => {
   margin: 0;
   display: -webkit-box;
   -webkit-line-clamp: 2;
+  line-clamp: 2;
   -webkit-box-orient: vertical;
   overflow: hidden;
   text-overflow: ellipsis;
